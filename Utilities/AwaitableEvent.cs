4a75b9b82f424ae130d177e52b76f1a731d231a6
Pete Mainardi <markpmainardi@gmail.com>
2025-04-22 19:53:07 -0400

    Support delegates of up to four arguments in AwaitableEvent

--- Utilities/AwaitableEvent.cs
+++ Utilities/AwaitableEvent.cs
@@ -65,7 +65,7 @@ namespace MainArtery.Utilities
         }
 
         /// <summary>
-        /// Unsubscribe sequential and nonsequential listeners
+        /// Unsubscribe sequential and nonsequential listeners.
         /// </summary>
         public async Task RemoveAllListeners()
         {
@@ -148,7 +148,262 @@ namespace MainArtery.Utilities
         }
 
         /// <summary>
-        /// Unsubscribe sequential and nonsequential listeners
+        /// Unsubscribe sequential and nonsequential listeners.
+        /// </summary>
+        public async Task RemoveAllListeners()
+        {
+            using (await _lock.LockAsync())
+            {
+                _sequential.Clear();
+                _unordered.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Call <see cref="RemoveAllListeners"/>.
+        /// </summary>
+        public void Dispose() => _ = RemoveAllListeners();
+
+        /// <summary>
+        /// Wait for <see cref="RemoveAllListeners"/>.
+        /// </summary>
+        public async ValueTask DisposeAsync() => await RemoveAllListeners();
+    }
+    /// ===========================================================================================
+    /// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    /// ===========================================================================================
+    /// <summary>
+    /// Generic implementation of an event whose invocation waits upon the execution of its
+    /// listeners, which receive two arguments from the event.
+    /// </summary>
+    /// ===========================================================================================
+    /// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    /// ===========================================================================================
+    public sealed class AwaitableEvent<T1, T2> : IAwaitableEvent<T1, T2>, IDisposable, IAsyncDisposable
+    {
+        private readonly List<Func<T1, T2, Task>> _sequential = new List<Func<T1, T2, Task>>();
+        private readonly List<Func<T1, T2, Task>> _unordered = new List<Func<T1, T2, Task>>();
+
+        private readonly AsyncLock _lock = new AsyncLock();
+
+        /// <summary>
+        /// Execute all listening functions.<br/>
+        /// Non-sequential listeners are all immediately executed, then sequential listeners are
+        /// executed, in order, after each successive one completes.
+        /// </summary>
+        /// <param name="arg1">The first event argument</param>
+        /// <param name="arg2">The second event argument</param>
+        /// <returns>The awaitable task</returns>
+        public async Task Invoke(T1 arg1, T2 arg2)
+        {
+            using (await _lock.LockAsync())
+            {
+                var unordered = Task.WhenAll(_unordered.Select(listener => listener(arg1, arg2)));
+                foreach (var listener in _sequential)
+                    await listener(arg1, arg2);
+                await unordered;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task AddSequentialListener(Func<T1, T2, Task> listener)
+        {
+            using (await _lock.LockAsync())
+                if (!_sequential.Contains(listener))
+                    _sequential.Add(listener);
+        }
+
+        /// <inheritdoc/>
+        public async Task AddNonSequentialListener(Func<T1, T2, Task> listener)
+        {
+            using (await _lock.LockAsync())
+                if (!_unordered.Contains(listener))
+                    _unordered.Add(listener);
+        }
+
+        /// <inheritdoc/>
+        public async Task RemoveListener(Func<T1, T2, Task> listener)
+        {
+            using (await _lock.LockAsync())
+            {
+                _sequential.Remove(listener);
+                _unordered.Remove(listener);
+            }
+        }
+
+        /// <summary>
+        /// Unsubscribe sequential and nonsequential listeners.
+        /// </summary>
+        public async Task RemoveAllListeners()
+        {
+            using (await _lock.LockAsync())
+            {
+                _sequential.Clear();
+                _unordered.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Call <see cref="RemoveAllListeners"/>.
+        /// </summary>
+        public void Dispose() => _ = RemoveAllListeners();
+
+        /// <summary>
+        /// Wait for <see cref="RemoveAllListeners"/>.
+        /// </summary>
+        public async ValueTask DisposeAsync() => await RemoveAllListeners();
+    }
+    /// ===========================================================================================
+    /// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    /// ===========================================================================================
+    /// <summary>
+    /// Generic implementation of an event whose invocation waits upon the execution of its
+    /// listeners, which receive three arguments from the event.
+    /// </summary>
+    /// ===========================================================================================
+    /// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    /// ===========================================================================================
+    public sealed class AwaitableEvent<T1, T2, T3> : IAwaitableEvent<T1, T2, T3>, IDisposable, IAsyncDisposable
+    {
+        private readonly List<Func<T1, T2, T3, Task>> _sequential = new List<Func<T1, T2, T3, Task>>();
+        private readonly List<Func<T1, T2, T3, Task>> _unordered = new List<Func<T1, T2, T3, Task>>();
+
+        private readonly AsyncLock _lock = new AsyncLock();
+
+        /// <summary>
+        /// Execute all listening functions.<br/>
+        /// Non-sequential listeners are all immediately executed, then sequential listeners are
+        /// executed, in order, after each successive one completes.
+        /// </summary>
+        /// <param name="arg1">The first event argument</param>
+        /// <param name="arg2">The second event argument</param>
+        /// <param name="arg3">The third event argument</param>
+        /// <returns>The awaitable task</returns>
+        public async Task Invoke(T1 arg1, T2 arg2, T3 arg3)
+        {
+            using (await _lock.LockAsync())
+            {
+                var unordered = Task.WhenAll(_unordered.Select(listener => listener(arg1, arg2, arg3)));
+                foreach (var listener in _sequential)
+                    await listener(arg1, arg2, arg3);
+                await unordered;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task AddSequentialListener(Func<T1, T2, T3, Task> listener)
+        {
+            using (await _lock.LockAsync())
+                if (!_sequential.Contains(listener))
+                    _sequential.Add(listener);
+        }
+
+        /// <inheritdoc/>
+        public async Task AddNonSequentialListener(Func<T1, T2, T3, Task> listener)
+        {
+            using (await _lock.LockAsync())
+                if (!_unordered.Contains(listener))
+                    _unordered.Add(listener);
+        }
+
+        /// <inheritdoc/>
+        public async Task RemoveListener(Func<T1, T2, T3, Task> listener)
+        {
+            using (await _lock.LockAsync())
+            {
+                _sequential.Remove(listener);
+                _unordered.Remove(listener);
+            }
+        }
+
+        /// <summary>
+        /// Unsubscribe sequential and nonsequential listeners.
+        /// </summary>
+        public async Task RemoveAllListeners()
+        {
+            using (await _lock.LockAsync())
+            {
+                _sequential.Clear();
+                _unordered.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Call <see cref="RemoveAllListeners"/>.
+        /// </summary>
+        public void Dispose() => _ = RemoveAllListeners();
+
+        /// <summary>
+        /// Wait for <see cref="RemoveAllListeners"/>.
+        /// </summary>
+        public async ValueTask DisposeAsync() => await RemoveAllListeners();
+    }
+    /// ===========================================================================================
+    /// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    /// ===========================================================================================
+    /// <summary>
+    /// Generic implementation of an event whose invocation waits upon the execution of its
+    /// listeners, which receive four arguments from the event.
+    /// </summary>
+    /// ===========================================================================================
+    /// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    /// ===========================================================================================
+    public sealed class AwaitableEvent<T1, T2, T3, T4> : IAwaitableEvent<T1, T2, T3, T4>, IDisposable, IAsyncDisposable
+    {
+        private readonly List<Func<T1, T2, T3, T4, Task>> _sequential = new List<Func<T1, T2, T3, T4, Task>>();
+        private readonly List<Func<T1, T2, T3, T4, Task>> _unordered = new List<Func<T1, T2, T3, T4, Task>>();
+
+        private readonly AsyncLock _lock = new AsyncLock();
+
+        /// <summary>
+        /// Execute all listening functions.<br/>
+        /// Non-sequential listeners are all immediately executed, then sequential listeners are
+        /// executed, in order, after each successive one completes.
+        /// </summary>
+        /// <param name="arg1">The first event argument</param>
+        /// <param name="arg2">The second event argument</param>
+        /// <param name="arg3">The third event argument</param>
+        /// <param name="arg4">The fourth event argument</param>
+        /// <returns>The awaitable task</returns>
+        public async Task Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            using (await _lock.LockAsync())
+            {
+                var unordered = Task.WhenAll(_unordered.Select(listener => listener(arg1, arg2, arg3, arg4)));
+                foreach (var listener in _sequential)
+                    await listener(arg1, arg2, arg3, arg4);
+                await unordered;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task AddSequentialListener(Func<T1, T2, T3, T4, Task> listener)
+        {
+            using (await _lock.LockAsync())
+                if (!_sequential.Contains(listener))
+                    _sequential.Add(listener);
+        }
+
+        /// <inheritdoc/>
+        public async Task AddNonSequentialListener(Func<T1, T2, T3, T4, Task> listener)
+        {
+            using (await _lock.LockAsync())
+                if (!_unordered.Contains(listener))
+                    _unordered.Add(listener);
+        }
+
+        /// <inheritdoc/>
+        public async Task RemoveListener(Func<T1, T2, T3, T4, Task> listener)
+        {
+            using (await _lock.LockAsync())
+            {
+                _sequential.Remove(listener);
+                _unordered.Remove(listener);
+            }
+        }
+
+        /// <summary>
+        /// Unsubscribe sequential and nonsequential listeners.
         /// </summary>
         public async Task RemoveAllListeners()
         {
--- Utilities/IAwaitableEvent.cs
+++ Utilities/IAwaitableEvent.cs
@@ -65,4 +65,106 @@ namespace MainArtery.Utilities
         /// </summary>
         Task RemoveListener(Func<T, Task> listener);
     }
+
+    /// <summary>
+    /// Public-facing interactions with an event whose invocation waits asynchronously upon the
+    /// execution of its listeners.
+    /// </summary>
+    /// <typeparam name="T1">The type of object to send as the first parameter to the event</typeparam>
+    /// <typeparam name="T2">The type of object to send as the second parameter to the event</typeparam>
+    /// <remarks>
+    /// The purpose of defining these operations in an interface is to allow restriction of the
+    /// event's invocation to the object that contains it, like plain C# events, e.g.:
+    /// <para/>
+    /// <code>
+    ///     private readonly <see cref="AwaitableEvent{T1, T2}"/> _thingChanged = new();
+    ///     public <see cref="IAwaitableEvent{T1, T2}"/> ThingChanged => _thingChanged;
+    /// </code>
+    /// </remarks>
+    public interface IAwaitableEvent<T1, T2>
+    {
+        /// <summary>
+        /// Add a listener that will execute in order after any previously added sequential listeners.
+        /// </summary>
+        Task AddSequentialListener(Func<T1, T2, Task> listener);
+
+        /// <summary>
+        /// Add a listener that will execute in concert with any previously added non-sequential listeners.
+        /// </summary>
+        Task AddNonSequentialListener(Func<T1, T2, Task> listener);
+
+        /// <summary>
+        /// Unsubscribe the listener from the event invocation.
+        /// </summary>
+        Task RemoveListener(Func<T1, T2, Task> listener);
+    }
+
+    /// <summary>
+    /// Public-facing interactions with an event whose invocation waits asynchronously upon the
+    /// execution of its listeners.
+    /// </summary>
+    /// <typeparam name="T1">The type of object to send as the first parameter to the event</typeparam>
+    /// <typeparam name="T2">The type of object to send as the second parameter to the event</typeparam>
+    /// <typeparam name="T3">The type of object to send as the third parameter to the event</typeparam>
+    /// <remarks>
+    /// The purpose of defining these operations in an interface is to allow restriction of the
+    /// event's invocation to the object that contains it, like plain C# events, e.g.:
+    /// <para/>
+    /// <code>
+    ///     private readonly <see cref="AwaitableEvent{T1, T2, T3}"/> _thingChanged = new();
+    ///     public <see cref="IAwaitableEvent{T1, T2, T3}"/> ThingChanged => _thingChanged;
+    /// </code>
+    /// </remarks>
+    public interface IAwaitableEvent<T1, T2, T3>
+    {
+        /// <summary>
+        /// Add a listener that will execute in order after any previously added sequential listeners.
+        /// </summary>
+        Task AddSequentialListener(Func<T1, T2, T3, Task> listener);
+
+        /// <summary>
+        /// Add a listener that will execute in concert with any previously added non-sequential listeners.
+        /// </summary>
+        Task AddNonSequentialListener(Func<T1, T2, T3, Task> listener);
+
+        /// <summary>
+        /// Unsubscribe the listener from the event invocation.
+        /// </summary>
+        Task RemoveListener(Func<T1, T2, T3, Task> listener);
+    }
+
+    /// <summary>
+    /// Public-facing interactions with an event whose invocation waits asynchronously upon the
+    /// execution of its listeners.
+    /// </summary>
+    /// <typeparam name="T1">The type of object to send as the first parameter to the event</typeparam>
+    /// <typeparam name="T2">The type of object to send as the second parameter to the event</typeparam>
+    /// <typeparam name="T3">The type of object to send as the third parameter to the event</typeparam>
+    /// <typeparam name="T4">The type of object to send as the fourth parameter to the event</typeparam>
+    /// <remarks>
+    /// The purpose of defining these operations in an interface is to allow restriction of the
+    /// event's invocation to the object that contains it, like plain C# events, e.g.:
+    /// <para/>
+    /// <code>
+    ///     private readonly <see cref="AwaitableEvent{T1, T2, T3, T4}"/> _thingChanged = new();
+    ///     public <see cref="IAwaitableEvent{T1, T2, T3, T4}"/> ThingChanged => _thingChanged;
+    /// </code>
+    /// </remarks>
+    public interface IAwaitableEvent<T1, T2, T3, T4>
+    {
+        /// <summary>
+        /// Add a listener that will execute in order after any previously added sequential listeners.
+        /// </summary>
+        Task AddSequentialListener(Func<T1, T2, T3, T4, Task> listener);
+
+        /// <summary>
+        /// Add a listener that will execute in concert with any previously added non-sequential listeners.
+        /// </summary>
+        Task AddNonSequentialListener(Func<T1, T2, T3, T4, Task> listener);
+
+        /// <summary>
+        /// Unsubscribe the listener from the event invocation.
+        /// </summary>
+        Task RemoveListener(Func<T1, T2, T3, T4, Task> listener);
+    }
 }
